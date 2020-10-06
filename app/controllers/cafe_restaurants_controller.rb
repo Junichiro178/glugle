bf0d4e17aa0116c6f20ff94bc9945696948dcd9d
Junichiro178 <bz.kitm.fj@gmail.com>
2020-10-06 22:58:07 +0900

    Set new & create action

--- app/controllers/cafe_restaurants_controller.rb
+++ app/controllers/cafe_restaurants_controller.rb
@@ -3,6 +3,33 @@ class CafeRestaurantsController < ApplicationController
   end
 
   def new
-    @CafeRestaurant = CafeRestaurant.new
+    @cafe_restaurant = CafeRestaurant.new
+  end
+
+  def create
+    @cafe_restaurant = CafeRestaurant.new(cafe_restaurant_params)
+    if @cafe_restaurant.save
+      redirect_to root_path
+    else
+      redirect_to root_path, alert: "お店を登録できませんでした"
+    end
+  end
+
+  private
+
+  def cafe_restaurant_params
+    params.require(:cafe_restaurant).permit(
+      :name,
+      :phonetic_name,
+      :phone_number, 
+      :description,
+      :opening_hours, 
+      :regular_holidays, 
+      :prefecture, 
+      :city_ward, 
+      :block_number, 
+      :building_name
+      )
+      .merge(owner_id: current_user.id)
   end
 end
