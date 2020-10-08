2b249dc5b7a3976182291ca16e69f63a60ddddf1
Junichiro178 <bz.kitm.fj@gmail.com>
2020-10-08 17:41:33 +0900

    Create seed.rb for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "create Categories"
+Category.create!(
+  name: 'グルテンフリー'
+)
+Category.create!(
+  name: 'ヴィーガン'
+)
+Category.create!(
+  name: 'オーガニック'
+)
+Category.create!(
+  name: '腸内環境'
+)
+Category.create!(
+  name: 'エシカル食品'
+)
+Category.create!(
+  name: 'アーユルヴェーダ'
+)
+Category.create!(
+  name: 'サステナブルビューティー'
+)
+Category.create!(
+  name: 'その他'
+)
