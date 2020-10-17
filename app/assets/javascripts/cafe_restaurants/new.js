b8d02915801650d5b39bdefba867998398522657
Junichiro178 <bz.kitm.fj@gmail.com>
2020-10-17 13:29:11 +0900

    in prodress to post multiple photos

---
+++ app/assets/javascripts/cafe_restaurants/new.js
@@ -0,0 +1,30 @@
+document.addEventListener("turbolinks:load", function(){
+  const buildFileField = (index)=> {
+    const html = `<div data-index="${index}" class="js-file_group">
+                    <input class="js-file" type="file" name="cafe_restaurant[images_attributes][${index}][image]" id="cafe_restaurant_images_attributes_${index}_image">
+                    <div class="js-remove">削除</div>
+                  </div>`;
+    return html;
+  }
+
+  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+  let imageBox = document.getElementById('image-box');
+  let upLoader = document.querySelector('.js-file');
+    // removeBtn = imageBox.querySelector('.js-remove');
+  
+  upLoader.addEventListener("change" ,function(e){
+    if (e.target.className ==='js-file') {
+      imageBox.insertAdjacentHTML('beforeend', buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    } 
+  });
+
+  console.log(document.querySelector('.js-file'));
+  // removeBtn.addEventListener("click", function(){
+  //   this.parent.remove();
+  //   console.log("success");
+  // });
+
+})
