1f95a8398041442e21b4fa8ccd325cf02dcd8348
Junichiro178 <bz.kitm.fj@gmail.com>
2020-10-17 13:54:49 +0900

    Cannot add photos if file count is more than 10

--- app/assets/javascripts/cafe_restaurants/new.js
+++ app/assets/javascripts/cafe_restaurants/new.js
@@ -10,9 +10,12 @@ document.addEventListener("turbolinks:load", function(){
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
   $('#image-box').on('change', '.js-file', function(e) {
-    $('#image-box').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    let count = $('.js-file').length;
+    if (count < 10) {
+      $('#image-box').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    }
   });
 
   $('#image-box').on('click', '.js-remove', function() {
