e5ff61a27a67e87631ec8ae3d97f4dc6360b258b
Junichiro178 <bz.kitm.fj@gmail.com>
2020-10-24 13:16:41 +0900

    changed into master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,8 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w{ config/secrets.yml }
+set :linked_files, %w{ config/master.key }
+
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
@@ -33,13 +34,13 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-  desc 'upload secrets.yml'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
