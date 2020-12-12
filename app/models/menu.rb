6ecd77afe728d815c1ce6dbd3d2938da1cceeb15
Junichiro178 <bz.kitm.fj@gmail.com>
2020-12-12 13:27:38 +0900

    Add association

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,6 @@
 class Menu < ApplicationRecord
   belongs_to :cafe_restaurant
-  has_many :menu_menu_categories
+  has_many :menu_menu_categories, dependent: :destroy
+  has_many :menu_categories, through: :menu_menu_categories
   validates :name, :price, :description, :image, presence: true
 end
