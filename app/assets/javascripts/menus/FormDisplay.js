04e5eb10f0021ddb8fbad833fb3cb01f210b4f62
Junichiro178 <bz.kitm.fj@gmail.com>
2020-12-12 18:02:12 +0900

    Dleted js file

--- app/assets/javascripts/menus/FormDisplay.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  var tab = document.querySelector('.js-tab-trigger');
-
-  tab.addEventListener('click', () => {
-    document.querySelector('.js-tab-target').classList.add("is-active")
-  });
-
-});
