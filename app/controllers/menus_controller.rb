cf12ac9f92e00817f96a249d875f81b7c8628794
Junichiro178 <bz.kitm.fj@gmail.com>
2020-12-12 11:20:16 +0900

    Add menu controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,22 @@
 class MenusController < ApplicationController
-  def create
+
+  def new
+    @menu = Menu.new
   end
+
+  def create
+    @menu = Menu.create(menu_params)
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(
+      :name,
+      :price,
+      :image, 
+      :description,
+      )
+      .merge(cafe_restaurant_id: params[:cafe_restaurant.id])
+  end
+
 end
