a511f707929d02507b0f9686ced1d11d3f025aa7
Junichiro178 <bz.kitm.fj@gmail.com>
2020-12-12 13:27:50 +0900

    Add assciation

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -1,3 +1,4 @@
 class MenuCategory < ApplicationRecord
-  has_many :menu_menu_categories
+  has_many :menu_menu_categories, dependent: :destroy
+  has_many :menus, through: :menu_menu_categories
 end
