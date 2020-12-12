86410767207c91f5f17008898ff67834d8e229ec
Junichiro178 <bz.kitm.fj@gmail.com>
2020-12-12 20:51:35 +0900

    Add imageuploader

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,6 @@ class Menu < ApplicationRecord
   belongs_to :cafe_restaurant
   has_many :menu_menu_categories, dependent: :destroy
   has_many :menu_categories, through: :menu_menu_categories
-  validates :name, :price, :description, :image, presence: true
+  validates :menu_categories, :name, :price, :description, :image, presence: true
+  mount_uploader :image, ImageUploader
 end
