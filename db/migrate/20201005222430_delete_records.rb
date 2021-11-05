618f795f487a748dc20f1352cbc352f03dd80a0b
Junichiro178 <bz.kitm.fj@gmail.com>
2021-11-05 14:11:08 +0900

    Update 20201005222430_delete_records.rb

--- db/migrate/20201005222430_delete_records.rb
+++ db/migrate/20201005222430_delete_records.rb
@@ -1,5 +1,5 @@
-class DeleteRecords < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :restaurant_cafes
-  end
-end
+# class DeleteRecords < ActiveRecord::Migration[5.2]
+#   def change
+#     drop_table :restaurant_cafes
+#   end
+# end
